a8e4e37de6149dc558ddbf8c5837be4c41bcdf36
kkawaharanet <kkawaharanet@gmail.com>
2025-02-25 20:14:41 +0900

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Link, Outlet } from "react-router-dom";
+import { Link, Outlet } from "react-router";
 import "./App.css";
 
 export default function App() {
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router";
 import App from "./App.tsx";
 import "./index.css";
 import { CsvToMarkdownTablePage } from "./pages/CsvToMarkdownTablePage.tsx";
